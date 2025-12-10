8f1a9a6556afd54e146528ad851fe9eeccc491c4
unknown <geetha272003@gmail.com>
2025-12-10 17:39:32 +0530

    update

--- applications/urls.py
+++ applications/urls.py
@@ -12,7 +12,8 @@ from .views import (
     RecruiterInternshipApplicantsView,
 
     UpdateJobApplicationStatusView,
-    UpdateInternshipApplicationStatusView
+    UpdateInternshipApplicationStatusView,
+    RecruiterOverviewView
     
     
 )
